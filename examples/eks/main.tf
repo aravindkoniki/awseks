9acc06b0794304f54013846ffef3b732400705d5
Aravind Koniki <aravind_koniki@hotmail.com>
2024-04-13 11:10:04 +0400

    Working on node deployment

--- examples/eks/main.tf
+++ examples/eks/main.tf
@@ -13,3 +13,43 @@ module "control_plane" {
     provider_key_arn = "${var.cluster_encryption_kms_key}"
   }
 }
+
+# user data module
+module "user_data" {
+  source               = "git::https://github.com/aravindkoniki/cloudinit_config.git?ref=main"
+  platform             = "bottlerocket"
+  cluster_name         = module.control_plane.cluster_name
+  cluster_endpoint     = module.control_plane.cluster_endpoint
+  cluster_ip_family    = module.control_plane.cluster_ip_family
+  cluster_service_cidr = module.control_plane.cluster_service_cidr
+
+  bootstrap_extra_args = <<-EOT
+    # extra args added
+    [settings.kernel]
+    lockdown = "integrity"
+
+    [settings.kubernetes.node-labels]
+    "label1" = "foo"
+    "label2" = "bar"
+  EOT
+
+}
+
+# module launch template
+module "eks_launch_template" {
+  source                 = "git::https://github.com/aravindkoniki/awslaunchtemplate.git?ref=main"
+  prefix                 = "eks"
+  description            = "EKS launchtemplate"
+  name                   = "eks-launchtemaplate"
+  user_data              = module.user_data.user_data
+  vpc_security_group_ids = var.cluster_security_group_id
+}
+
+# module eks nodes
+module "self_managed_nodes" {
+  source             = "../../module/nodes/eks_managed_node_group"
+  ami_type           = "BOTTLEROCKET_x86_64"
+  launch_template_id = module.eks_launch_template.id
+  cluster_name       = module.control_plane.cluster_name
+  subnet_ids         = ["subnet-0b190b6bb8a5788cd", "subnet-01dac7b80252dea0d", "subnet-06856317c3478f3b6"]
+}
