b6a9d2fbc8ee86b4ae49fadc955ef8b14706e53e
Aravind Koniki <aravind_koniki@hotmail.com>
2024-04-14 11:52:53 +0400

    Working on nodess deplpyment issue

---
+++ examples/eks/data.tf
@@ -0,0 +1,9 @@
+data "aws_ami" "bottlerocket_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["bottlerocket-aws-k8s-${local.cluster_version}-x86_64-*"]
+  }
+}
